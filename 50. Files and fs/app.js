c3f9f6440c5a1aa1d1d7e49da6cdfd7e7880c1b7
blank <blankngnl@someplace.com>
2019-06-05 22:12:23 +0200

    50.

---
+++ 50. Files and fs/app.js
@@ -0,0 +1,11 @@
+let fs = require('fs');
+
+let greet = fs.readFileSync(__dirname + '/greet.txt', 'utf8');
+console.log(greet);
+
+let greet2 = fs.readFileSync(__dirname + '/greet.txt','utf8',
+    function (err,data) {
+        console.log(data);
+    });
+console.log(greet2);
+console.log('Done!');
