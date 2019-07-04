527cb87e0c1afcfe963d1cf9919a4aec9b149bf7
Blank <blankngnl@protonmail.com>
2019-07-04 17:12:16 +0200

    75.

---
+++ 75. Static Files and Middleware/app.js
@@ -0,0 +1,24 @@
+let express = require('express');
+let app = express();
+
+let port = process.env.PORT || 3000;
+
+app.use('/assets', express.static(__dirname + '/public'));  //Middleware(uploads/downloads)
+
+app.use('/', function (req,res,next) {
+    console.log('Requested URL: ' + req.url);
+    next();
+});
+
+app.get('/',function (req,res) {
+    res.send('<html><head><link href=assets/style.css type=text/css rel=stylesheet /></head><body><h1>Hello World </h1></body></html>');
+});
+
+app.get('/person/:id',function (req,res) {
+    res.send('<html><head><body><h1>Person:' + req.params.id+ '</h1></body></head></html>')
+});
+
+app.get('/api',function (req,res) {
+    res.json({firstname: 'Noella', lastname:'Schaap'})
+});
+app.listen(port);
